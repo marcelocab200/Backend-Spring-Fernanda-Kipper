f2600328372122def066aa580c29583fa1d4b7d7
marcelocab200 <marcelocab200@gmail.com>
2025-03-02 16:55:48 -0300

    update: repositories methods and event entity address relation

--- src/main/java/com/eventostec/api/domain/event/Event.java
+++ src/main/java/com/eventostec/api/domain/event/Event.java
@@ -1,8 +1,10 @@
 package com.eventostec.api.domain.event;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,6 +13,8 @@ import lombok.Setter;
 
 import java.util.Date;
 import java.util.UUID;
+
+import com.eventostec.api.domain.address.Address;
 
 @Table(name = "event")
 @Entity
@@ -30,4 +34,7 @@ public class Event {
     private String eventUrl;
     private Boolean remote;
     private Date date;
+
+    @OneToOne(mappedBy = "event", cascade = CascadeType.ALL)
+    private Address address;
 }

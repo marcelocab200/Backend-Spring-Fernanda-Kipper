76a30f3fde2a1670807ab3f7b41c2c08439a384a
marcelocab200 <marcelocab200@gmail.com>
2025-03-02 16:54:53 -0300

    feat: add services

---
+++ src/main/java/com/eventostec/api/service/AddressService.java
@@ -0,0 +1,23 @@
+package com.eventostec.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.eventostec.api.domain.address.Address;
+import com.eventostec.api.domain.event.Event;
+import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.repositories.AddressRepository;
+
+public class AddressService {
+    
+    @Autowired
+    private AddressRepository addressRepository;
+
+    public Address createAddress(EventRequestDTO data, Event event) {
+        Address address = new Address();
+        address.setCity(data.city());
+        address.setUf(data.state());
+        address.setEvent(event);
+
+        return addressRepository.save(address);
+    }
+}
